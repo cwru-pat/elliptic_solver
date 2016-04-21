6175339ff8fe581505a6bb3ce1301149c691d680
rectaflex <rectaflex@gmail.com>
2016-04-20 21:22:55 -0400

    Update multigrid.cc

--- multigrid.cc
+++ multigrid.cc
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
 
   std::cout << "Creating multigrid class...\n";
   idx_t min_depth = 2;
-  idx_t max_depth = 7;
+  idx_t max_depth = 6;
   real_t grid_length = 1.0;
   FASMultigrid<real_t, idx_t> multigrid (max_depth, min_depth, grid_length);
   std::cout << "  initializing...\n";
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
   std::cout << "  done.\n";
 
   std::cout << "Performing V-Cycles...\n";
-  multigrid.VCycles(10);
+  multigrid.VCycles(1); //performing inexact Newton interation, take numer of V-Cycles as 1 as test
   std::cout << "  done.\n";
 
   exit(EXIT_SUCCESS);
