7cd32548d57ba49e5d1042f5fbbdad3773da7c6c
Chris Tian <chris@farin.phys.cwru.edu>
2016-05-17 14:32:06 -0400

    multigrid with relaxation method control

--- main.cpp
+++ main.cpp
@@ -20,13 +20,15 @@ int main(int argc, char **argv)
   idx_t min_depth = 2;
   idx_t max_depth = 7;
   real_t grid_length = 1.0;
-  FASMultigrid<real_t, idx_t> multigrid (max_depth, min_depth, grid_length, 5);
+  idx_t iter_num = 5;
+  idx_t relax_scheme = 2;
+  FASMultigrid<real_t, idx_t> multigrid (max_depth, min_depth, grid_length, iter_num, relax_scheme);
   std::cout << "  initializing...\n";
   multigrid.add_poly_srcs(0); 
   multigrid.setTrialSolution();
   std::cout << "  done.\n";
   std::cout << "Performing V-Cycles...\n";
-  multigrid.VCycles(10);
+  multigrid.VCycles(6);
   std::cout << "  done.\n";
   
   exit(EXIT_SUCCESS);
