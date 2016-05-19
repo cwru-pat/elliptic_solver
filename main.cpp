c67bdd2bd601aafb37dc79bcb08af22792018b43
rectaflex <rectaflex@gmail.com>
2016-05-19 12:50:04 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,26 +22,28 @@ int main(int argc, char **argv)
   
   real_t grid_length = 1.0;
   idx_t iter_num = 5;
-  // relax_scheme =  0 for damping inexact Newton; 1 for damping inexact Newton with constraint to speed up;
+  // relax_scheme =  0 for damping inexact Newton;
+  //1 for damping inexact Newton with constraint to speed up;
   // 2 for regular Newton interation
-  idx_t relax_scheme = 1;
+  idx_t relax_scheme = 0;
   idx_t grid_length_x = 1, grid_length_y = 1, grid_length_z = 1;
 
   //set grid number of different direction, usually we require: grid_num_i % (2^iter_depth) == 0
   //to maintain fast convergent rate.
   //But the code actually can deal with arbitrary grid number at any direction.
-  idx_t grid_num_x = 64, grid_num_y = 80, grid_num_z = 88;
-  idx_t iter_depth = 4; //number of layers for multigrid interation
+  idx_t grid_num_x = 64, grid_num_y = 64, grid_num_z = 64;
+  idx_t iter_depth = 5; //number of layers for multigrid interation
 
   FASMultigrid<real_t, idx_t> multigrid (grid_num_x, grid_num_y, grid_num_z,
   grid_length_x, grid_length_y, grid_length_z, iter_depth, iter_num, relax_scheme);
   //FASMultigrid<real_t, idx_t> multigrid (max_depth, min_depth, 1, iter_num, relax_scheme);
   std::cout << "  initializing...\n";
-  multigrid.add_poly_srcs(0); 
-  multigrid.setTrialSolution();
+  multigrid.setTrialSolution(1);
+  multigrid.add_poly_srcs(1); 
+
   std::cout << "  done.\n";
   std::cout << "Performing V-Cycles...\n";
-  multigrid.VCycles(10);
+  multigrid.VCycles(5);
   std::cout << "  done.\n";
   
   exit(EXIT_SUCCESS);
